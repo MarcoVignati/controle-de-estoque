81f3aac6904832a295d755750c92f5f8395fe0b0
Marco Vignati <marcovignatidev@gmail.com>
2025-01-31 10:02:27 -0300

    feat(model): adiciona classe Fornecedor

---
+++ src/main/java/com/vignati/controle_de_estoque/model/Fornecedor.java
@@ -0,0 +1,74 @@
+package com.vignati.controle_de_estoque.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class Fornecedor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @NotBlank
+    private String nome;
+
+    @NotBlank
+    private String telefone;
+
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String endereco;
+
+    public Fornecedor() {
+
+    }
+
+    public Fornecedor(String nome, String telefone, String email, String endereco) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+        this.endereco = endereco;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
